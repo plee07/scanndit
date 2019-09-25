0fc34e9cd276c96d448f451b5fd01f96a4e233e3
Phil <lee.phil@outlook.com>
2019-09-25 17:00:06 -0400

    added homepage loadmore/pagination

--- js/index.js
+++ js/index.js
@@ -1,11 +1,14 @@
+const POSTS_PER_PAGE = 25;
+
+
 function postClick(event) {
   console.log(this.dataset.postId);
 }
 
 function appendToHomepageFeed(data, page) {
   let allPosts = document.querySelector('.all-posts');
-  let i1 = ((page - 1) * 25)
-  let i2 = (page * 25);
+  let i1 = ((page - 1) * POSTS_PER_PAGE)
+  let i2 = (page * POSTS_PER_PAGE);
   let pageOfPosts = data.slice(i1,i2);
   let postFeed = document.querySelector('.all-posts');
   for (post of pageOfPosts) {
@@ -23,4 +26,13 @@ window.addEventListener('DOMContentLoaded', (event) => {
   window.pagesDisplayed = 0;
   getAllPosts()
     .then(data => appendToHomepageFeed(data, window.pagesDisplayed + 1));
+
+  const loadMoreButton = document.querySelector('.load-more-button');
+  loadMoreButton.addEventListener('click',()=>{
+    getAllPosts()
+    .then(data => appendToHomepageFeed(data, window.pagesDisplayed + 1));
+  });
+
+
 });
+
