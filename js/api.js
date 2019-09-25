f843d8b7e800a6d90959f8fb39b8296263393c67
Phil <lee.phil@outlook.com>
2019-09-25 17:43:58 -0400

    added getallcomment

--- js/api.js
+++ js/api.js
@@ -7,3 +7,32 @@ async function getAllPosts()
   let data = await response.json()
   return data;
 }
+
+// loading all comments based on comment id
+async function getAllComments(postId){
+  let response = await fetch(`${API_ENDPOINT_BASE}post/${postId}/comment`);
+  let data = await response.json();
+  return data;
+}
+
+const newUser = {
+  "email" : "pan@test200.com",
+  "password" : "test5012",
+  "username" : "pat"
+}
+// New User signup
+async function postNewUser(newUser){
+  let response = await fetch(`${API_ENDPOINT_BASE}post/signup`, {
+      method: 'POST',
+      headers:{
+          'Accept': 'application/json',
+          'Content-Type': 'application/json'
+      },
+      body: JSON.stringify(newUser)
+  })
+  let token = await response.json();
+  return token;
+}
+
+postNewUser(newUser).then(res => console.log(res));
+
